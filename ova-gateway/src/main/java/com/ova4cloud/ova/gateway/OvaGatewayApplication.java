8728af206795810d9b85cecd57f109c0719ce5b1
JYFChina <643905028@qq.com>
2021-05-11 09:44:49 +0800

    初始化

---
+++ ova-gateway/src/main/java/com/ova4cloud/ova/gateway/OvaGatewayApplication.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 pig4cloud Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ova4cloud.ova.gateway;
+
+import com.ova4cloud.ova.common.swagger.annotation.EnablePigSwagger2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lengleng
+ * @date 2018年06月21日
+ * <p>
+ * 网关应用
+ */
+@EnablePigSwagger2
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OvaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvaGatewayApplication.class, args);
+	}
+
+}
